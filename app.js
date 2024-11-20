309f9171da64bb91283274b38f07e70ab9cace41
joefelx <joefelix.a2003@gmail.com>
2024-11-20 11:13:17 +0530

    Reinitialized

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const bcrpt = require("bcrypt");
+const bcrpt = require("bcryptjs");
 const User = require("./models/User");
 const app = express();
 const saltRounds = 10;
-const currentuser = {};
+let currentuser = {};
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,13 +14,14 @@ app.use(express.static("public"));
 
 dotenv.config();
 
-mongoose.connect(
-  process.env.MONGOURI,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(process.env.MONGOURI)
+  .then(() => {
     console.log("MongoDB Connected!!");
-  }
-);
+  })
+  .catch((e) => console.log(e));
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -54,32 +55,37 @@ app.get("/user/:id", async (req, res) => {
 
 app.post("/register", async (req, res) => {
   try {
+    console.log(req.body);
     let username = req.body.username;
     const email = req.body.email;
     const balance = req.body.balance;
     const salt = await bcrpt.genSalt(saltRounds);
     const hashedPassword = await bcrpt.hash(req.body.password, salt);
 
-    const newUser = await User({
+    const newUser = new User({
       name: username.toLowerCase(),
       email: email,
       password: hashedPassword,
       balance: balance,
     });
+
+    console.log(newUser);
+
     const user = await newUser.save();
     currentuser = user;
+
+    return res.redirect("/alluser");
   } catch (error) {
     console.log(error);
   }
-
-  res.redirect("/alluser");
 });
 
 app.post("/transaction", async (req, res) => {
   const amount = parseInt(req.body.amount);
-  const sender = await User.findOne({ name: req.body.sender.toLowerCase() });
+
+  const sender = await User.findOne({ email: req.body.sender.toLowerCase() });
   const receiver = await User.findOne({
-    name: req.body.receiver.toLowerCase(),
+    email: req.body.receiver.toLowerCase(),
   });
   const senderbalance = sender.balance;
   const receiverbalance = receiver.balance;
@@ -89,17 +95,17 @@ app.post("/transaction", async (req, res) => {
     await sender.updateOne({ balance: senderbalance - amount });
     await receiver.updateOne({ balance: receiverbalance + amount });
     console.log(success);
-    res.redirect("/alluser");
+    return res.redirect("/alluser");
   } catch (err) {
     console.log(err);
     console.log(fail);
-    res.redirect("/transaction");
+    return res.redirect("/transaction");
   }
 });
 
-const port = process.env.PORT;
+let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 3000;
+  port = 5000;
 }
 
 app.listen(port, () => {
